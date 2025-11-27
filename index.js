b2e8e593c7b6ecf6bed634a818e5765cfa784dbc
Carlos Leal <carlosleal2342@gmail.com>
2025-11-26 20:22:49 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hola mundo");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
---
+++ models/User.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  identityCard: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
